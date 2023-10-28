88087aec179f26876cf06a652d2c73f8ca4a20a8
SacHin Bhalerao <97786039+Sachbhalerao70@users.noreply.github.com>
2023-10-28 05:49:06 +0000

    02_DataType

---
+++ 01_basic/02_dataType.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all js code as newer version 
+// alert("hello") // i m using node js in that this syntax not working let see  not browser
+
+
+let name="sach"
+let number=50
+let isLog;
+
+/* premitive datatype
+number ,Bigint ,string boolean ,null(stand alone value,empty value)
+undefined (value not decide)
+symbol
+non premetive 
+object
+
+*/
+console.log(typeof(isLog))
+// null ka output object
---
+++ 01_basic/variable.js
@@ -0,0 +1,15 @@
+const accountId = 155485
+let accountEmail= "sach@gmail.com"
+var accountPass= "12345"
+accountCity="sambhajinagar"
+let accountState;
+// accountId=5 not allowed 
+console.log(accountId);
+accountEmail="Sach"
+accountPass="78"
+accountCity="jay"
+console.table([accountId,accountEmail,accountCity,accountPass,accountState])
+/*
+Prefer to not used var 
+becouse of issue in block scope and functional scope
+*/
