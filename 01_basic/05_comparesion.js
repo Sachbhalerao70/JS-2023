c2d375da995839cf9439855a988af64b4fc1bf4d
SacHin Bhalerao <97786039+Sachbhalerao70@users.noreply.github.com>
2023-10-29 05:26:56 +0000

    comparison operator

---
+++ 01_basic/05_comparesion.js
@@ -0,0 +1,29 @@
+// console.log(2>1);
+// console.log(2>=1);
+
+// console.log("2">1);
+
+// console.log("2s">1);
+
+// console.log(null>0);
+// console.log(null>=0);
+
+// console.log(null<0);
+// console.log(null<=0);
+// console.log(null==0)
+
+
+// console.log(null!= 0);
+
+// comparasion convert null to number trating as 0
+//null >=0 is true and null>0 = false
+  
+
+// console.log(undefined>0);
+// console.log(undefined>=0);
+// console.log(undefined<=0);
+// console.log(undefined==0);
+
+//  ===
+console.log("2"==2);
+console.log("2"===2);
