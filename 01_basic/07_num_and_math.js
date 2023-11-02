60ce9d0f5088bc05df6a40db4f89ed6a7cdcf8cc
SacHin Bhalerao <97786039+Sachbhalerao70@users.noreply.github.com>
2023-11-02 05:26:03 +0000

    num_and_ maths

---
+++ 01_basic/07_num_and_math.js
@@ -0,0 +1,36 @@
+// const score=400
+// console.log(score);
+
+// const balance=new Number(475)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(2));// 100.00 precision value   ex 100.0000 
+
+// const otherNum=452.1652
+
+// console.log(otherNum.toPrecision(3)); //toPrecision= use for to round a value
+
+// const zero=1000000000
+// console.log(zero.toLocaleString('en-IN')); //give a comma inside zero
+
+
+//+++++++   Maths  +++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-5)); // only just convert -value to postive 
+console.log(Math.round(4.6666));
+console.log(Math.ceil(4.1));//top value
+console.log(Math.floor(4.9)); // base value 
+
+console.log(Math.min(4,5,6,8,2,1));
+
+
+console.log(Math.max(4,5,6,8,2,1));
+
+console.log(Math.random(0,100));
+
+
+
+
