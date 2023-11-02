7cde4c5f0463c44f3271d346dea14be1da40b667
SacHin Bhalerao <97786039+Sachbhalerao70@users.noreply.github.com>
2023-11-02 05:25:48 +0000

    Strings

---
+++ 01_basic/06_string.js
@@ -0,0 +1,38 @@
+// const name="Sachin"
+
+// const count=18
+
+// console.log(name+count+"bahlerao");// dont used this syntax
+
+// console.log(`Hello My name is ${name} and my age is ${count}`); // this syntax called string interpolation 
+
+
+// const gameName= new String("ABCD")
+// console.log(gameName );
+// console.log(gameName[2]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+
+// console.log(gameName.slice(1));
+
+
+// console.log(gameName.toLowerCase());
+
+// console.log(gameName.charAt(1));
+
+const myName="    Sachin-Bhalerao"
+// const newString=myName.substring(0,5)
+
+// const newString=myName.slice(-2)
+
+// console.log(newString);
+
+
+console.log(myName.trim());
+console.log(myName.replace('-','hii'));
+
+console.log(myName.includes("Sachin"));
+
+// convert to array
+
+console.log(myName.trim().split('-'));
